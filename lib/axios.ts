a79fb2989cc36e557c5b82f1d499c0d771b92744
Ferdiyanto Deni Sanjaya <Ferdiyanto.Deni@daihatsu.astra.co.id>
2026-04-07 10:59:16 +0700

    Fix: Hardcode baseURL API Alquran

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://api.alquran.cloud/v1/",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
 })
 
 export default axiosInstance;
