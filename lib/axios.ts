7005f7faaf45e6fa61e97da0277c4ed8ef357e67
Ferdiyanto Deni Sanjaya <Ferdiyanto.Deni@daihatsu.astra.co.id>
2026-04-08 14:49:06 +0700

    del eslin add user agent

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.9',
+    },
 })
 
 export default axiosInstance;
--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // Opsional: agar build tidak berhenti jika ada error tipe data kecil
   },
-  eslint: {
-    ignoreDuringBuilds: true, // Opsional: agar build tidak berhenti karena aturan penulisan (linting)
-  }
 };
 
 export default nextConfig;
