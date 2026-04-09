9ee31c54444ab3f3bd6d66c6a3beae8b97170daa
Ferdiyanto Deni Sanjaya <Ferdiyanto.Deni@daihatsu.astra.co.id>
2026-04-09 15:20:22 +0700

    update surah api direct

--- hooks/useFetchSurah.tsx
+++ hooks/useFetchSurah.tsx
@@ -1,11 +1,12 @@
 import axiosInstance from "@/lib/axios";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export default function useFetchSurah() {
     return useQuery({
         queryKey: ["surah-list"],
         queryFn: async () => {
-            const response = await axiosInstance.get("/surah");
+            const response = await axiosInstance.get("https://api.alquran.cloud/v1/surah");
             return response?.data?.data;
         },
         staleTime: Infinity,
